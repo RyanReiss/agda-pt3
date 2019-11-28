ed467aaff863a4dddf0ba2edc8700179987a95f7
STC-gda <57202576+STC-gda@users.noreply.github.com>
2019-11-27 16:17:47 -0800

    Add files via upload

---
+++ agda-pt3/WeaponStat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//In progess
+public  class WeaponStat : Gun
+{
+    //A class that manage statistics of weapons
+    float weaponRange;
+    float weaponDamage;
+    float fireRate;
+    float reloadTime;
+    float velocicy;
+  
+}
