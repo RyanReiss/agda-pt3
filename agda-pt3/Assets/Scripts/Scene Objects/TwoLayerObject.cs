f62137e812c5bf1211ff4c4194d9a1eeacd2fa8d
RyanReiss <rreiss1208@gmail.com>
2020-01-30 18:25:51 -0800

    Added Dynamic Trees

---
+++ agda-pt3/Assets/Scripts/Scene Objects/TwoLayerObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoLayerObject : MonoBehaviour
+{
+    public string frontLayerName;
+    public string backLayerName;
+    private int frontLayer;
+    private int backLayer;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        frontLayer = SortingLayer.GetLayerValueFromName(frontLayerName);
+        backLayer = SortingLayer.GetLayerValueFromName(backLayerName);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.position.y <= this.transform.position.y && transform.GetChild(0).GetComponent<SpriteRenderer>().sortingLayerName == frontLayerName){
+            // The player is in front of the object
+            Debug.Log("Swapped to back");
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sortingLayerName = backLayerName;
+        } else if(player.position.y > this.transform.position.y && transform.GetChild(0).GetComponent<SpriteRenderer>().sortingLayerName == backLayerName){
+            Debug.Log("Swapped to front");
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sortingLayerName = frontLayerName;
+        }
+    }
+}
--- agda-pt3/Assets/Scripts/Weapons/BulletEffect/Physical/Pushback.cs
+++ agda-pt3/Assets/Scripts/Weapons/BulletEffect/Physical/Pushback.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Pushback : Effect {
     public override void triggerEffect (GameObject bullet, Collider2D obj, float k) {
 
-        if (obj.transform.name != "Player" && obj.transform.tag != "TriggersToIgnore") {
+        if (obj.transform.name != "Player" && obj.transform.tag != "TriggersToIgnore" && obj.transform.tag != "MapObjects") {
             if (obj.transform.tag != "Map")
                 obj.transform.position += bullet.transform.up.normalized * Time.deltaTime * k;
             // obj.position += obj.up.normalized * Time.deltaTime * k;
